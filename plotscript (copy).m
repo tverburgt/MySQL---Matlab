0c94ae4bcee60b4d4435e846dc28059cd2e3e7d0
Ion Comendant <ion564@debian.ion564>
2015-05-06 13:19:59 +0100

    Plot function created

---
+++ plotscript (copy).m
@@ -0,0 +1,18 @@
+%By Ion Comendant, Tyrone Verburgt and Assylzhan Mussin
+%ctave script that plots a graph of temperature vs time obtained from the MySQL database
+
+
+data=dlmread("plot.csv",",");  	%reads an ASCII-delimited numeric data file into matrix M. 
+								%The dlmread function detects the delimiter from the file 
+								%and treats repeated white spaces as a single delimiter
+
+
+x=data(:,1);					%inserting 1st column data into the x varaible
+y=data(:,2);					%inserting 2n column vectors i.e. temperature in the y variable
+
+
+%title('Graph of Temperature Vs Time(2 second delay)');
+%xlabel('Time (seconds)');
+%ylabel('Temperature (Celcius)');
+
+plot(x,y)
